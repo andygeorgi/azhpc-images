f5f3daa36ce8147de84c45539d7d4a57fe1f637b
Andy Georgi <Andy.Georgi@microsoft.com>
2022-04-05 13:38:21 +0200

    Update MLNX OFED version for Alma 8.5

--- alma/alma-8.x/alma-8.5-hpc/install_mellanoxofed.sh
+++ alma/alma-8.x/alma-8.5-hpc/install_mellanoxofed.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -ex
 
-MLNX_OFED_DOWNLOAD_URL=https://azhpcstor.blob.core.windows.net/azhpc-images-store/MLNX_OFED_LINUX-5.2-1.0.4.0-rhel8.3-x86_64.tgz
+MLNX_OFED_DOWNLOAD_URL=https://content.mellanox.com/ofed/MLNX_OFED-5.5-1.0.3.2/MLNX_OFED_LINUX-5.5-1.0.3.2-rhel8.5-x86_64.tgz
 TARBALL=$(basename ${MLNX_OFED_DOWNLOAD_URL})
 MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 
-$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "dd13ffa54524af9509e3de7288a863aa48ed7016bb3d15446523703020c41715"
+$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "f6327d7671be4836f97cabb0c9dd87895adbf28b4cb68be58e019f6a6ea49d68"
 tar zxvf ${TARBALL}
 
 KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
 KERNEL=${KERNEL[-1]}
 yum install -y kernel-devel-${KERNEL}
-./${MOFED_FOLDER}/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/${KERNEL} --add-kernel-support --skip-repo
-
+./${MOFED_FOLDER}/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/${KERNEL} --add-kernel-support --skip-repo --distro rhel8.5
