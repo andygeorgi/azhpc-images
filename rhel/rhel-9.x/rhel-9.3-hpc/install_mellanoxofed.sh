593701cc63c301bd303f6253c928db4fbaaa60d8
Andy Georgi <Andy.Georgi@microsoft.com>
2024-01-17 18:16:57 +0100

    Fixed kernel and distro version

--- rhel/rhel-9.x/rhel-9.3-hpc/install_mellanoxofed.sh
+++ rhel/rhel-9.x/rhel-9.3-hpc/install_mellanoxofed.sh
@@ -9,9 +9,8 @@ MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 $COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "ff59becc07d6803f9a71a995f2c0c268d6006b94e46b24b4178742f4935050de"
 tar zxvf ${TARBALL}
 
-KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
-KERNEL=${KERNEL[-1]}
-./${MOFED_FOLDER}/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/${KERNEL} --add-kernel-support --skip-repo --skip-unsupported-devices-check --without-fw-update --distro rhel9.2
+KERNEL=$(uname -r)
+./${MOFED_FOLDER}/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/${KERNEL} --add-kernel-support --skip-repo --skip-unsupported-devices-check --without-fw-update --distro rhel9.3
 
 # Issue: Module mlx5_ib belong to a kernel which is not a part of MLNX
 # Resolution: set FORCE=1/ force-restart /etc/init.d/openibd 
